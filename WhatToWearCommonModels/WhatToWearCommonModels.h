6091130766acdf0078a8c47563d4bf5f77caa344
Blair McArthur <blair.mcarthur@skipthedishes.com>
2018-04-11 17:54:34 +1200

    Initial Commit

---
+++ WhatToWearCommonModels/WhatToWearCommonModels.h
@@ -0,0 +1,19 @@
+//
+//  WhatToWearCommonModels.h
+//  WhatToWearCommonModels
+//
+//  Created by Blair McArthur on 11/04/18.
+//  Copyright © 2018 Noobish1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhatToWearCommonModels.
+FOUNDATION_EXPORT double WhatToWearCommonModelsVersionNumber;
+
+//! Project version string for WhatToWearCommonModels.
+FOUNDATION_EXPORT const unsigned char WhatToWearCommonModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatToWearCommonModels/PublicHeader.h>
+
+
