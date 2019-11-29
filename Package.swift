280e69377a6ba86d50a6bfb0f0d906c3c6504623
Blair McArthur <blair.mcarthur@icloud.com>
2019-11-28 23:02:44 -0500

    fixed the Package.swift deployment targets

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ let name = "WhatToWearCommonModels"
 
 let package = Package(
     name: name,
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_14)
+    ],
     products: [.library(name: name, targets: [name])],
     dependencies: [
         .package(url: "https://github.com/Noobish1/whattowearcommoncore.git", from: "2.1.0"),
