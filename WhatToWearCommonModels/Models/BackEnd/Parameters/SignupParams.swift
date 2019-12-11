5a397bb893abb4e8e86c88c5a404051017c34a26
Blair McArthur <blair.mcarthur@icloud.com>
2019-12-11 02:07:47 -0500

    Added all the stored settings to SignupParams

--- WhatToWearCommonModels/Models/BackEnd/Parameters/SignupParams.swift
+++ WhatToWearCommonModels/Models/BackEnd/Parameters/SignupParams.swift
@@ -4,10 +4,25 @@ public struct SignupParams: Codable {
     // MARK: properties
     public let email: String
     public let password: String
+    public let globalSettings: GlobalSettings
+    public let timeSettings: TimeSettings
+    public let storedRules: StoredRules
+    public let storedLocations: StoredLocations?
     
     // MARK: init
-    public init(email: String, password: String) {
+    public init(
+        email: String,
+        password: String,
+        globalSettings: GlobalSettings,
+        timeSettings: TimeSettings,
+        storedRules: StoredRules,
+        storedLocations: StoredLocations?
+    ) {
         self.email = email
         self.password = password
+        self.globalSettings = globalSettings
+        self.timeSettings = timeSettings
+        self.storedRules = storedRules
+        self.storedLocations = storedLocations
     }
 }
