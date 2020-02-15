57106f4e3c0eb3e8a7fcca8cacaf5f74ea5ba464
Blair McArthur <blair.mcarthur@icloud.com>
2020-02-15 14:47:31 -0500

    updated the UserResponse model to include all the things

--- WhatToWearCommonModels/Models/BackEnd/Responses/UserResponse.swift
+++ WhatToWearCommonModels/Models/BackEnd/Responses/UserResponse.swift
@@ -7,9 +7,14 @@ public struct UserResponse: Codable, Equatable {
     public let firstName: String
     public let lastName: String
     public let email: String
+    public let identifierForVendor: String?
     public let callsMade: Int
     public let callLimit: Int
     public let verified: Bool
+    public let globalSettings: GlobalSettings
+    public let storedLocations: StoredLocations?
+    public let storedRules: StoredRules
+    public let timeSettings: TimeSettings
     
     // MARK: init
     public init(
@@ -17,16 +22,26 @@ public struct UserResponse: Codable, Equatable {
         firstName: String,
         lastName: String,
         email: String,
+        identifierForVendor: String?,
         callsMade: Int,
         callLimit: Int,
-        verified: Bool
+        verified: Bool,
+        globalSettings: GlobalSettings,
+        storedLocations: StoredLocations?,
+        storedRules: StoredRules,
+        timeSettings: TimeSettings
     ) {
         self.id = id
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
+        self.identifierForVendor = identifierForVendor
         self.callsMade = callsMade
         self.callLimit = callLimit
         self.verified = verified
+        self.globalSettings = globalSettings
+        self.storedLocations = storedLocations
+        self.storedRules = storedRules
+        self.timeSettings = timeSettings
     }
 }
