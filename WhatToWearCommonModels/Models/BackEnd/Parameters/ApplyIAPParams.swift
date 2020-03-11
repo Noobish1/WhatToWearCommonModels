4a66083a81a3a06d93956b1b253513dc66a258e0
Blair McArthur <blair.mcarthur@icloud.com>
2020-03-11 16:44:13 -0400

    using a NonEmptyArray in ApplyIAPParams

--- WhatToWearCommonModels/Models/BackEnd/Parameters/ApplyIAPParams.swift
+++ WhatToWearCommonModels/Models/BackEnd/Parameters/ApplyIAPParams.swift
@@ -1,11 +1,12 @@
 import Foundation
+import WhatToWearCommonCore
 
 public struct ApplyIAPParams: Codable {
     // MARK: properties
-    public let productIDs: [String]
+    public let productIDs: NonEmptyArray<String>
     
     // MARK: init
-    public init(productIDs: [String]) {
+    public init(productIDs: NonEmptyArray<String>) {
         self.productIDs = productIDs
     }
 }
